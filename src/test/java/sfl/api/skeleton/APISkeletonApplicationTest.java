0d4e1b8c7e52566c7ffd60fa7a2b8eda150388a8
alberto.sfolcini <alberto.sfolcini@host.docker.internal>
2021-01-05 12:22:23 +0100

    initial

---
+++ src/test/java/sfl/api/skeleton/APISkeletonApplicationTest.java
@@ -0,0 +1,14 @@
+package sfl.api.skeleton;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class APISkeletonApplicationTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/sfl/api/skeleton/APISkeletonControllerTest.java
@@ -0,0 +1,14 @@
+package sfl.api.skeleton;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class APISkeletonControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
